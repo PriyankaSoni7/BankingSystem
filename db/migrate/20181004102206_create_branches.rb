05794f37501c1de41b834592b2bd76e82c383009
PriyankaSoni7 <priyanka01472@gmail.com>
2018-10-04 18:50:16 +0530

    20181004102206_create_branches.rb

---
+++ db/migrate/20181004102206_create_branches.rb
@@ -0,0 +1,10 @@
+class CreateBranches < ActiveRecord::Migration[5.2]
+  def change
+    create_table :branches do |t|
+      t.string :address
+      t.references :bank, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
